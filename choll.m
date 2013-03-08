1e17eab4d4ac6b7ca756a2a7c4e6b6ce7ad622a3
Minjie Xu <chokkyvista06@gmail.com>
2013-03-08 09:45:35 +0800

    more aggressive augmentation

--- choll.m
+++ choll.m
@@ -7,17 +7,19 @@
 
 function [q,P] = choll(A)
 P = A;
+% m = max(min(abs(P(:)))*(1e-6), 1e-12);
 m = 1e-12;
 [q,s] = chol(P);
 diagind = 1:size(P,1)+1:numel(P);
 cc = 0;
 while s~=0
-    P(diagind) = P(diagind) + m;
+    P(diagind) = A(diagind) + m;
     cc = cc + 1;
+    m = m * 2;
     [q,s] = chol(P);
 end
 if cc > 0
-    warning('Augmented %d times by %.4f\n', cc, cc*m);
+    warning('Augmented %d times by %.4f\n', cc, m);
 end
 
 end
